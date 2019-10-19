bfd452dcb470eb52edf56a8442bcdc05999144e8
awayui <yuiyuiawayui@gmail.com>
2019-10-20 03:10:48 +0900

    Collect some parameters into "parameter.hh"

---
+++ include/parameter.hh
@@ -0,0 +1,10 @@
+﻿namespace parameter
+{
+    // att. length of sci-cube
+    G4double absPS[] = {
+        38. * cm, 38. * cm, 38. * cm, 38. * cm, 38. * cm, 38. * cm, 38. * cm, 38. * cm, 38. * cm, 38. * cm,
+        38. * cm, 38. * cm, 38. * cm, 38. * cm, 38. * cm, 38. * cm, 38. * cm, 38. * cm, 38. * cm, 38. * cm,
+        38. * cm, 38. * cm, 38. * cm, 38. * cm, 38. * cm, 38. * cm, 38. * cm, 38. * cm, 38. * cm, 38. * cm,
+        38. * cm, 38. * cm, 38. * cm, 38. * cm, 38. * cm, 38. * cm, 38. * cm, 38. * cm, 38. * cm, 38. * cm,
+        38. * cm, 38. * cm, 38. * cm, 38. * cm, 38. * cm, 38. * cm, 38. * cm, 38. * cm, 38. * cm, 38. * cm };
+}
